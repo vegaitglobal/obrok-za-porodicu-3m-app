92609d3434936be40c293e4432f613ab65e33dbc
Ivana Vlaisavljevic <i.vlaisavljevic@vegait.rs>
2022-10-15 01:06:48 +0200

    Radio button and radio buttons row added

--- mob/src/screens/ActionsListScreen.tsx
+++ mob/src/screens/ActionsListScreen.tsx
@@ -1,80 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-import {Field, Formik} from 'formik';
-import {
-  Keyboard,
-  SafeAreaView,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
-import FormikTextInput from '../components/atoms/Formik/FormikTextInput/FormikTextInput';
-import {testSchema} from '../validation/TestSchema';
-import FormikDropdown from '../components/atoms/Formik/FormikDropdown/FormikDropdown';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 const ActionsListScreen = () => {
-  const initialValues = {
-    comment: '',
-    nesto: '',
-  };
-  const handleOnDeclinePress = (comment: string) => {
-    console.log(comment);
-  };
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [loseFocus, setLoseFocus] = useState(false);
-
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}>
-      <SafeAreaView style={styles.container}>
-        <Formik
-          validationSchema={testSchema}
-          initialValues={initialValues}
-          enableReinitialize
-          onSubmit={values => {
-            handleOnDeclinePress(values.comment);
-          }}>
-          {({}) => (
-            <View style={styles.container}>
-              <Field
-                validateOnChange
-                component={FormikTextInput}
-                loseFocus={loseFocus}
-                editable
-                name={'comment'}
-                label={'Ime i Prezime'}
-                placeholder={'Ime i Prezime'}
-              />
-              <Field
-                validateOnChange
-                component={FormikTextInput}
-                loseFocus={loseFocus}
-                editable
-                name={'nesto'}
-                textArea
-                label={'Ime i Prezime'}
-                placeholder={'Ime i Prezime'}
-              />
-              <Field
-                validateOnChange
-                component={FormikDropdown}
-                loseFocus={loseFocus}
-                editable
-                submitOnChange={true}
-                dropdownValues={[
-                  {label: 'sss', value: 2},
-                  {label: 'ddad', value: 33},
-                ]}
-                name={'selectedCreator'}
-                label={'Select Creator'}
-              />
-            </View>
-          )}
-        </Formik>
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
+    <SafeAreaView style={styles.container}>
+      <Text>ActionsListScreen</Text>
+    </SafeAreaView>
   );
 };
 
@@ -83,6 +15,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+    width: '100%',
   },
   formik: {
     flex: 1,
