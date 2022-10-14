065f3f7a95b1ba2033f7aa74ecfd190734fbd5e6
Ivana Vlaisavljevic <i.vlaisavljevic@vegait.rs>
2022-10-14 21:17:07 +0200

    Icons file added

---
+++ mob/src/constants/Icons.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import ArrowDown from '../../assets/icons/arrow_left.svg';
+import ArrowLeft from '../../assets/icons/arrow_left.svg';
+import CallFull from '../../assets/icons/call_full.svg';
+import Call from '../../assets/icons/call.svg';
+import Contact from '../../assets/icons/contact.svg';
+import Donate from '../../assets/icons/donate.svg';
+import FileFull from '../../assets/icons/file_full.svg';
+import File from '../../assets/icons/file.svg';
+import Filter from '../../assets/icons/filter.svg';
+import HomeFull from '../../assets/icons/home_full.svg';
+import Home from '../../assets/icons/home.svg';
+import NotificationFull from '../../assets/icons/notification_full.svg';
+import Notification from '../../assets/icons/notification.svg';
+import Play from '../../assets/icons/play.svg';
+import Search from '../../assets/icons/search.svg';
+import Share from '../../assets/icons/share.svg';
+import Star from '../../assets/icons/star.svg';
+import UserFull from '../../assets/icons/user_full.svg';
+import User from '../../assets/icons/user.svg';
+import Warning from '../../assets/icons/warning.svg';
+
+export default {
+  ARROW_DOWN: <ArrowDown />,
+  ARROW_LEFT: <ArrowLeft />,
+  CALL: <Call />,
+  CALL_FULL: <CallFull />,
+  CONTACT: <Contact />,
+  DONATE: <Donate />,
+  FILE: <File />,
+  FILE_FULL: <FileFull />,
+  FILTER: <Filter />,
+  HOME_FULL: <HomeFull />,
+  HOME: <Home />,
+  NOTIFICATION_FULL: <NotificationFull />,
+  NOTIFICATION: <Notification />,
+  PLAY: <Play />,
+  SEARCH: <Search />,
+  SHARE: <Share />,
+  STAR: <Star />,
+  USER_FULL: <UserFull />,
+  USER: <User />,
+  WARNING: <Warning />,
+};
