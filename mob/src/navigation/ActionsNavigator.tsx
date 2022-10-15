c91cb9ab28fa41a294007c32a743d4c802b83065
Mihajlo Dzever <m.dzever@vegait.rs>
2022-10-16 00:46:34 +0200

    [MOB-134] Removed filter from screens and refactoring

--- mob/src/navigation/ActionsNavigator.tsx
+++ mob/src/navigation/ActionsNavigator.tsx
@@ -1,45 +1,28 @@
 import React from 'react';
 
-import {
-  createStackNavigator,
-  StackNavigationOptions,
-} from '@react-navigation/stack';
-import {useRoute} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {AppRoute} from './Routes';
 
 import ActionScreen from '../screens/ActionScreen';
 import ActionsListScreen from '../screens/ActionsListScreen';
 
-import OPHeader from '../components/organisms/OPHeader/OPHeader';
+import {actionScreenOptions, actionsStackOptions} from './screenOptions';
 
 const Stack = createStackNavigator();
 
 const ActionsNavigator = () => {
-  const route = useRoute();
-  const screenOptions: StackNavigationOptions = {
-    header: () => (
-      <OPHeader
-        hasFilter={route.name === AppRoute.ACTIONS_NAVIGATOR ? true : false}
-      />
-    ),
-  };
   return (
     <Stack.Navigator
       initialRouteName={AppRoute.ACTIONS_LIST_SCREEN}
-      screenOptions={screenOptions}>
+      screenOptions={actionsStackOptions}>
       <Stack.Screen
         name={AppRoute.ACTIONS_LIST_SCREEN}
         component={ActionsListScreen}
-        options={{
-          headerShown: false,
-        }}
       />
       <Stack.Screen
         name={AppRoute.ACTION_SCREEN}
         component={ActionScreen}
-        options={{
-          headerShown: true,
-        }}
+        options={actionScreenOptions}
       />
     </Stack.Navigator>
   );
