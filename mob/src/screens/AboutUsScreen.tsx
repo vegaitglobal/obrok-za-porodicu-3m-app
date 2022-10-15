a92d7b79e3cf07a174f198ea50cf552a8d553f51
Milan Paunovic <m.paunovic@vegait.rs>
2022-10-15 15:33:29 +0200

    Subscribe box added to page

--- mob/src/screens/AboutUsScreen.tsx
+++ mob/src/screens/AboutUsScreen.tsx
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   StyleSheet,
   useWindowDimensions,
+  View,
   ViewStyle,
 } from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
@@ -13,6 +14,7 @@ import RenderHtml from 'react-native-render-html';
 import {RootState} from '../store/reducers/RootReducer';
 import {useAppThunkDispatch} from '../store/Store';
 import {TextStyles} from '../constants/TextStyles';
+import OPNewsletterSubscribe from '../components/molecules/OPNewsletterSubscribe/OPNewsletterSubscribe';
 
 const AboutUsScreen = () => {
   const {html} = useSelector((state: RootState) => state.aboutUs);
@@ -25,29 +27,33 @@ const AboutUsScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scroll}>
-        <RenderHtml
-          enableExperimentalGhostLinesPrevention={true}
-          enableExperimentalBRCollapsing={true}
-          source={{html: html}}
-          contentWidth={width}
-          systemFonts={[
-            TextStyles.DOSIS_REGULAR.fontFamily!,
-            TextStyles.DOSIS_EXTRA_BOLD.fontFamily!,
-          ]}
-          ignoredDomTags={['br']}
-          tagsStyles={{
-            p: {
-              fontSize: 16,
-              fontFamily: TextStyles.DOSIS_REGULAR.fontFamily,
-              fontWeight: 'normal',
-            },
-            h1: {
-              fontSize: 18,
-              fontFamily: TextStyles.DOSIS_EXTRA_BOLD.fontFamily,
-              fontWeight: '800',
-            },
-          }}
-        />
+        <View style={styles.htmlContainer}>
+          <RenderHtml
+            enableExperimentalGhostLinesPrevention={true}
+            enableExperimentalBRCollapsing={true}
+            source={{html: html}}
+            contentWidth={width}
+            systemFonts={[
+              TextStyles.DOSIS_REGULAR.fontFamily!,
+              TextStyles.DOSIS_EXTRA_BOLD.fontFamily!,
+            ]}
+            ignoredDomTags={['br']}
+            tagsStyles={{
+              p: {
+                fontSize: 16,
+                fontFamily: TextStyles.DOSIS_REGULAR.fontFamily,
+                fontWeight: 'normal',
+              },
+              h1: {
+                fontSize: 18,
+                fontFamily: TextStyles.DOSIS_EXTRA_BOLD.fontFamily,
+                fontWeight: '800',
+              },
+            }}
+          />
+        </View>
+
+        <OPNewsletterSubscribe />
       </ScrollView>
     </SafeAreaView>
   );
@@ -55,7 +61,8 @@ const AboutUsScreen = () => {
 
 const styles = StyleSheet.create({
   container: {} as ViewStyle,
-  scroll: {paddingBottom: 60, paddingLeft: 16, paddingRight: 16} as ViewStyle,
+  scroll: {paddingBottom: 60} as ViewStyle,
+  htmlContainer: {paddingLeft: 16, paddingRight: 16} as ViewStyle,
 });
 
 export default AboutUsScreen;
