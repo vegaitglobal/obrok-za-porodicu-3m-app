ac0237ab3aef7d302c9f694394a3956dd9d924de
Ksenija Prćić <109222948+k-prcic-vegait@users.noreply.github.com>
2022-10-15 18:53:45 +0200

    feat: connect with BE

--- fe/src/components/pages/contacts/ContactsPage.tsx
+++ fe/src/components/pages/contacts/ContactsPage.tsx
@@ -1,30 +1,31 @@
 import classes from "./ContactsPage.module.scss";
 import Table from "../../UI/molecules/table/Table";
 import Header from "../../UI/molecules/header/OPHeader";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
+import { useEffect } from "react";
+import { getContacts } from "../../../store/actions/contactTypes";
 
-const data: any = [
-  {id: 1, title: "Obrok za porodicu", email: "obrokzaporodicu@gmail.com", phone: "+381 69 123 45 67"},
-  {id: 2, title: "Obrok za porodicu", email: "obrokzaporodicu@gmail.com", phone: "+381 69 123 45 67"},
-  {id: 3, title: "Obrok za porodicu", email: "obrokzaporodicu@gmail.com", phone: "+381 69 123 45 67"},
-  {id: 4, title: "Obrok za porodicu", email: "obrokzaporodicu@gmail.com", phone: "+381 69 123 45 67"},
-]
+const headers: string[] = ["Title", "Email", "Phone number", "Actions"];
 
-const headers: string[] = [
-  "Title", "Email", "Phone"
-]
-
-const columnsToRender: string[] = [
-  "title", "email", "phone"
-]
+const columnsToRender: string[] = ["title", "email", "phoneNumber"];
 
 const ActionTypesPage = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector((state: RootState) => state.contacts.contacts);
+
+  useEffect(() => {
+    dispatch(getContacts());
+  }, []);
   return (
     <div className={classes["contacts-page"]}>
       <div className={classes["content-wrapper"]}>
         <div className={classes["header-wrapper"]}>
           <Header />
         </div>
-        <Table headers={headers} data={data} columns={columnsToRender} />
+        <div className={classes["table-wrapper"]}>
+          <Table headers={headers} data={contacts} columns={columnsToRender} />
+        </div>
       </div>
     </div>
   );
