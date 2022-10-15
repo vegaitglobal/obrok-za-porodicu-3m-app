7798d00e01795d44d45b8ac9c4eecb36440d360e
v-kovacevic-vegait <v.kovacevic@vegait.rs>
2022-10-15 15:02:09 +0200

    action types and contacts pages

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -3,6 +3,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/pages/login/LoginPage";
 import NotFoundPage from "./components/pages/notFound/NotFoundPage";
 import VolunteerActionsPage from "./components/pages/actions/VolunteerActionsPage";
+import ActionTypesPage from "./components/pages/actionTypes/ActionTypesPage";
+import ContactsPage from "./components/pages/contacts/ContactsPage";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/volunteer-action" element={<VolunteerActionsPage />} />
+      <Route path="/volunteer-action-type" element={<ActionTypesPage />} />
+      <Route path="/contact" element={<ContactsPage />} />
 
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
