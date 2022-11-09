f8acf8100b102c0d57752843d1c382c82ea87d03
amilovic <amilovic@aleksamilovic-mac.local>
2022-11-09 12:17:52 +0100

    Add email sending service

--- be/Program.cs
+++ be/Program.cs
@@ -53,6 +53,13 @@ builder.Services.AddTransient<IDonationRepository, DonationRepository>();
 builder.Services.AddTransient<IUserRepository, UserRepository>();
 builder.Services.AddTransient<IUserService, UserService>();
 
+builder.Services.AddTransient<IEmailSendingService, EmailSendingService>();
+
+var emailConfig = builder.Configuration
+        .GetSection("EmailConfiguration")
+        .Get<EmailConfiguration>();
+builder.Services.AddSingleton(emailConfig);
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddDbContext<DataContext>(options =>
---
+++ be/Services/EmailSendingService.cs
@@ -0,0 +1,54 @@
+using MealForFamily.ServiceInterface;
+using MealForFamily.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace MealForFamily.Service
+{
+    public class EmailSendingService : IEmailSendingService
+    {
+        private readonly EmailConfiguration _emailConfig;
+        public EmailSendingService(EmailConfiguration emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+            Send(emailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("3M", _emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = string.Format("<h2 style='color:red;'>{0}</h2>", message.Content) };
+            return emailMessage;
+        }
+        private void Send(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+                    client.Send(mailMessage);
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
