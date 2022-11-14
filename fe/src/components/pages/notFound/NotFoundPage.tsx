e1dd8b683e48b6ea53ac583f9db715454d6fa887
l-vuckovic-vegait <40487012+l-vuckovic-vegait@users.noreply.github.com>
2022-11-14 10:10:04 +0100

    Feature/fe 163 unsubscribe page (#128)
    * Create email templates
    
    * Create unsubscribe and 404 pages

--- fe/src/components/pages/notFound/NotFoundPage.tsx
+++ fe/src/components/pages/notFound/NotFoundPage.tsx
@@ -1,5 +1,12 @@
+import globalClasses from "../../../constants/GlobalStyle.module.scss";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className={globalClasses["page-wrapper"]} style={{width: "100vw", height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+      <h1>404</h1>
+      <h4>Stranica nije pronađena</h4>
+    </div>
+  );
 };
 
 export default NotFoundPage;
